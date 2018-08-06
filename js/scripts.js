d44a808786e730f7c80e202cb474c7e9d027e9ee
Grace J Adamiec and Miles Bradt <grace@>
2018-08-06 14:31:20 -0700

    create celsius to fahrenheit calculator

--- js/scripts.js
+++ js/scripts.js
@@ -1,49 +1,22 @@
-// var add = function(number1, number2){
-//   return number1 + number2
-// };
+//BMI Calculator
+// var bmi = function(weight, height){
+//   return  weight / (height**2) * 703
+// }
 //
-// var number1 = parseInt(prompt("Enter a number:"));
-// var number2 = parseInt(prompt("Enter another number:"));
-// var result = add(number1, number2);
-// alert(result);
+// var weight = parseInt(prompt("weight"));
+// var height = parseInt(prompt("height"));
 //
-// var subtract = function (number1, number2) {
-//   return number1 - number2
-// };
-//
-// var number1 = parseInt(prompt("number subtract)"))
-// var number2 = parseInt(prompt("number subtract)"))
-// var result = subtract(number1, number2);
-//
-// alert(result);
-//
-// var multiply = function (number1, number2) {
-//   return number1 * number2
-// };
-//
-// var number1 = parseInt(prompt("number multiply")))
-// var number2 = parseInt(prompt("number multiply")))
-// var result = multiply(number1, number2);
-//
-// alert(result);
-//
-// var divide = function (number1, number2) {
-//   return number1 * number2
-// };
-//
-// var number1 = parseInt(prompt("number divide")))
-// var number2 = parseInt(prompt("number divide")))
-// var result = divide(number1, number2);
+// var result = bmi(weight, height);
 //
 // alert(result);
 
-var bmi = function(weight, height){
-  return  weight / (height**2) * 703
-}
+//Celsius to Freedom
+var fahrenheit = function(celsius){
+  return celsius * (9/5) + 32
+};
 
-var weight = parseInt(prompt("weight"));
-var height = parseInt(prompt("height"));
+var celsius = parseInt(prompt("Temp in celsius, please: "));
 
-var result = bmi(weight, height);
+var result = fahrenheit(celsius);
 
 alert(result);
